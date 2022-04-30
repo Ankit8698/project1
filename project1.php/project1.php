5024c1a971e0fb9d2840cd55b95ffc9ceb5fd722
Ankit8698 <104663083+Ankit8698@users.noreply.github.com>
2022-04-30 08:35:55 +0530

    Add files via upload

---
+++ project1.php/project1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>    
+    <h1>Mumbai Corporation of Greater Mumbai</h1>
+  <div class="main">
+  
+        <div class="heading">
+            <h2>EMPLOYEE SALARY SLIP 2022</h2>
+         </div>
+
+    <div class="input">
+        Employee id: <br>
+        <input type="text"><br><br>
+        Month: <br>
+        <input type="text"> <br><br>
+    </div>
+
+    <a href="download.php?file=Salary slip02" ><button class="btn">Conform</button></a>
+
+  </div>  
+</body>
+</html>
